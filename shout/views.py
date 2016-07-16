78a09b5a218ddd6dcf45c69f544b88e5088a1972
Isley Gao <m059889@A4774816.local>
2016-07-15 22:51:55 -0700

    maps loads

--- shout/views.py
+++ shout/views.py
@@ -8,4 +8,6 @@ def index():
 @app.route('/hello/')
 @app.route('/hello/<name>')
 def hello(name=None):
-    return render_template('hello.html', name=name)
+    return render_template('map.html', name=name)
+
+
