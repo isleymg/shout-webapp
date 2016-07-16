6ef1805abc8d679cf15248556dff85ca66c451c4
Isley Gao <m059889@A4774816.local>
2016-07-16 09:16:17 -0700

    added cornell data points

--- shout/models/markers.py
+++ shout/models/markers.py
@@ -1,5 +1,19 @@
 # map_points: data_id, date, category, latitude, longitude
 
+from datetime import datetime
+
+
+def convert_date(datestring):
+	return datetime.strptime(datestring, '%b %d %Y %I:%M%p')
 
 
 
+def convert_to_json(lineEntry):
+	
+
+with open('data.txt') as f:
+	lines = f.readlines()
+	print lines
+
+for entry in lines:
+	convert_to_json(entry)
